8b6ead652a5843695e27f87b90f464c36f6cf380
Dmitry <135172784+Dmitry-Alekseev@users.noreply.github.com>
2026-05-22 19:54:14 +0300

    Update app to version 1.5

--- CrewPortal/app/src/main/java/com/example/crewportal/ui/theme/Color.kt
+++
@@ -1,8 +0,0 @@
-package com.example.crewportal.ui.theme
-
-import androidx.compose.ui.graphics.Color
-
-val ThaiPurple = Color(0xFF4B0082)
-val ThaiPurpleLight = Color(0xFFF4ECFF)
-val SuccessGreen = Color(0xFF0B8F2A)
-val TextMuted = Color(0xFF5F6270)
